f6f46b976e610121212f8914638276ca3b0d49fa
Davide Ghiotto <davide.ghiotto@bitrock.it>
2024-09-14 13:01:56 +0200

    better loader

--- src/pages/Reservations/LoaderReservations.tsx
+++ src/pages/Reservations/LoaderReservations.tsx
@@ -1,7 +1,8 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
-export function LoaderReservations() {
-  return (
+export function LoaderReservations({ count = 5 }: { count?: number }) {
+  const reservations = Array.from({ length: count });
+  return reservations.map(() => (
     <div className="flex items-center space-x-4">
       <Skeleton className="h-12 w-12 rounded-full" />
       <div className="space-y-2">
@@ -9,5 +10,5 @@ export function LoaderReservations() {
         <Skeleton className="h-4 w-[200px]" />
       </div>
     </div>
-  );
+  ));
 }
