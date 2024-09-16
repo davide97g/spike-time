69a3fd97d7da33074eb516ed5af647af128a1c00
Davide Ghiotto <davide.ghiotto@bitrock.it>
2024-09-16 13:00:36 +0200

    revert path

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -1,29 +1,9 @@
 import dotenv from "dotenv";
 import admin from "firebase-admin";
-import fs from "fs";
 
 dotenv.config();
 
-const serviceAccount = process.env.SECRETS_PATH
-  ? `${process.env.SECRETS_PATH}/service-account.json`
-  : "service-account.json";
-
-// log process dir
-console.log("cwd", process.cwd());
-
-// log folders in cwd
-fs.readdir(process.cwd(), (err: any, files: any) => {
-  console.log("cwd --> files:", files);
-});
-
-// log all files in the secrets path
-fs.readdir(
-  process.env.SECRETS_PATH ?? process.cwd(),
-  (err: any, files: any) => {
-    // log all files
-    console.log("secrets --> files", files);
-  }
-);
+const serviceAccount = `${process.env.SECRETS_PATH}/service-account.json`;
 
 export const initializeFirebaseApp = () => {
   try {
