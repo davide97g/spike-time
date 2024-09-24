6c9d5304cfa71f4d6b5e984b5fa52bcc1b60fd0d
Davide Ghiotto <davide.ghiotto@bitrock.it>
2024-09-24 22:16:46 +0200

    husky in root project

--- frontend/src/stories/slot.stories.tsx
+++ frontend/src/stories/slot.stories.tsx
@@ -1,6 +1,5 @@
 import { Slot } from "@/components/custom/Slot";
 import type { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
 import dayjs from "dayjs";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
@@ -22,8 +21,6 @@ const meta = {
       },
     },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
 } satisfies Meta<typeof Slot>;
 
 export default meta;
